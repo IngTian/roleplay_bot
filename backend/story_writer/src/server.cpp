1c90b6021f49ac21c93d0e19245e7882cd31a1f3
Zeying Tian <tzy20001018@gmail.com>
2025-01-30 15:49:56 -0800

    feat: minor changes

--- backend/story_writer/src/server.cpp
+++ backend/story_writer/src/server.cpp
@@ -16,7 +16,6 @@ class CalculatorServiceImpl final : public roleplay_bot::ai::StoryWriter::Servic
         const roleplay_bot::ai::InitializeSessionRequest *request,
         roleplay_bot::ai::InitializeSessionResponse *response
     ) -> Status override {
-        // TODO: Implement the service.
         spdlog::info("InitializeSession called");
         const std::string session_id = "100";
         response->set_session_id(session_id);
@@ -28,7 +27,6 @@ class CalculatorServiceImpl final : public roleplay_bot::ai::StoryWriter::Servic
         const roleplay_bot::ai::GetSessionConfigRequest *request,
         roleplay_bot::ai::GetSessionConfigResponse *response
     ) -> Status override {
-        // TODO: Implement the service.
         return Status::OK;
     }
 
@@ -37,7 +35,6 @@ class CalculatorServiceImpl final : public roleplay_bot::ai::StoryWriter::Servic
         const roleplay_bot::ai::WriteStoryRequest *request,
         roleplay_bot::ai::WriteStoryResponse *response
     ) -> Status override {
-        // TODO: Implement the service.
         return Status::OK;
     }
 
@@ -46,7 +43,6 @@ class CalculatorServiceImpl final : public roleplay_bot::ai::StoryWriter::Servic
         const roleplay_bot::ai::GetStoryHistoryRequest *request,
         roleplay_bot::ai::GetStoryHistoryResponse *response
     ) -> Status override {
-        // TODO: Implement the service.
         return Status::OK;
     }
 
@@ -55,24 +51,29 @@ class CalculatorServiceImpl final : public roleplay_bot::ai::StoryWriter::Servic
         const roleplay_bot::ai::DeleteSessionRequest *request,
         roleplay_bot::ai::DeleteSessionResponse *response
     ) -> Status override {
-        // TODO: Implement the service.
         return Status::OK;
     }
 };
 
-void RunServer() {
-    const std::string server_address("0.0.0.0:50051");
-    CalculatorServiceImpl service;
+namespace {
+    void RunServer() {
+        const std::string server_address("0.0.0.0:50051");
+        CalculatorServiceImpl service;
 
-    ServerBuilder builder;
-    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    builder.RegisterService(&service);
-    const std::unique_ptr<Server> server(builder.BuildAndStart());
-    std::cout << "Server listening on: " << server_address << '\n';
-    server->Wait();
-}
+        ServerBuilder builder;
+        builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+        builder.RegisterService(&service);
+        const std::unique_ptr<Server> server(builder.BuildAndStart());
+        std::cout << "Server listening on: " << server_address << '\n';
+        server->Wait();
+    }
+} // namespace
 
 auto main() -> int {
-    RunServer();
+    try {
+        RunServer();
+    } catch (const std::exception &e) {
+        spdlog::error("system crashed due to: {}", e.what());
+    }
     return 0;
 }
