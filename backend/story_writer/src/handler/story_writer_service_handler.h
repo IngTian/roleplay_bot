277c4febfd81660ead1b0b64a91090919254fe2a
Zeying Tian <tzy20001018@gmail.com>
2025-01-31 14:04:27 -0800

    refactor: moved server endpoints definition to the handler class

---
+++ backend/story_writer/src/handler/story_writer_service_handler.h
@@ -0,0 +1,44 @@
+//
+// Created by Beowulf Hrothgar on 2025-01-31.
+//
+
+#ifndef STORYWRITERSERVICEHANDLER_H
+#define STORYWRITERSERVICEHANDLER_H
+#include <story_writer.grpc.pb.h>
+
+namespace handler {
+    class StoryWriterServiceHandler final : public roleplay_bot::ai::StoryWriter::Service {
+    public:
+        auto InitializeSession(
+            grpc::ServerContext *context,
+            const roleplay_bot::ai::InitializeSessionRequest *request,
+            roleplay_bot::ai::InitializeSessionResponse *response
+        ) -> grpc::Status override;
+
+        auto GetSessionConfig(
+            grpc::ServerContext *context,
+            const roleplay_bot::ai::GetSessionConfigRequest *request,
+            roleplay_bot::ai::GetSessionConfigResponse *response
+        ) -> grpc::Status override;
+
+        auto WriteStory(
+            grpc::ServerContext *context,
+            const roleplay_bot::ai::WriteStoryRequest *request,
+            roleplay_bot::ai::WriteStoryResponse *response
+        ) -> grpc::Status override;
+
+        auto GetStoryHistory(
+            grpc::ServerContext *context,
+            const roleplay_bot::ai::GetStoryHistoryRequest *request,
+            roleplay_bot::ai::GetStoryHistoryResponse *response
+        ) -> grpc::Status override;
+
+        auto DeleteSession(
+            grpc::ServerContext *context,
+            const roleplay_bot::ai::DeleteSessionRequest *request,
+            roleplay_bot::ai::DeleteSessionResponse *response
+        ) -> grpc::Status override;
+    };
+} // namespace handler
+
+#endif //STORYWRITERSERVICEHANDLER_H
--- backend/story_writer/src/server.cpp
+++ backend/story_writer/src/server.cpp
@@ -1,64 +1,16 @@
 #include <iostream>
 #include <string>
 #include <grpcpp/grpcpp.h>
-#include "story_writer.grpc.pb.h"
+#include "handler/story_writer_service_handler.h"
 #include "spdlog/spdlog.h"
 
 using grpc::Server;
 using grpc::ServerBuilder;
-using grpc::ServerContext;
-using grpc::Status;
-
-// Service Implementation
-class CalculatorServiceImpl final : public roleplay_bot::ai::StoryWriter::Service {
-    auto InitializeSession(
-        ServerContext *context,
-        const roleplay_bot::ai::InitializeSessionRequest *request,
-        roleplay_bot::ai::InitializeSessionResponse *response
-    ) -> Status override {
-        spdlog::info("InitializeSession called");
-        const std::string session_id = "100";
-        response->set_session_id(session_id);
-        return Status::OK;
-    }
-
-    auto GetSessionConfig(
-        ServerContext *context,
-        const roleplay_bot::ai::GetSessionConfigRequest *request,
-        roleplay_bot::ai::GetSessionConfigResponse *response
-    ) -> Status override {
-        return Status::OK;
-    }
-
-    auto WriteStory(
-        ServerContext *context,
-        const roleplay_bot::ai::WriteStoryRequest *request,
-        roleplay_bot::ai::WriteStoryResponse *response
-    ) -> Status override {
-        return Status::OK;
-    }
-
-    auto GetStoryHistory(
-        ServerContext *context,
-        const roleplay_bot::ai::GetStoryHistoryRequest *request,
-        roleplay_bot::ai::GetStoryHistoryResponse *response
-    ) -> Status override {
-        return Status::OK;
-    }
-
-    auto DeleteSession(
-        ServerContext *context,
-        const roleplay_bot::ai::DeleteSessionRequest *request,
-        roleplay_bot::ai::DeleteSessionResponse *response
-    ) -> Status override {
-        return Status::OK;
-    }
-};
 
 namespace {
     void RunServer() {
         const std::string server_address("0.0.0.0:50051");
-        CalculatorServiceImpl service;
+        handler::StoryWriterServiceHandler service;
 
         ServerBuilder builder;
         builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
